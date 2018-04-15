5c49a89e52f012bce02e2a5872fafc9bec650c03
WIN-GJG0T70AG7E\Administrator <328007406@qq.com>
2018-04-15 23:04:26 +0800

    增加新闻页面

---
+++ restaurant/Application/Runtime/Cache/Home/02d9721f4f895d8a83125946ea3bc303.php
@@ -0,0 +1,106 @@
+<?php if (!defined('THINK_PATH')) exit();?><!DOCTYPE html>
+<html>
+<head>
+	<title>首页</title>
+	<link rel="stylesheet" type="text/css" href="/restaurant1/restaurant/Public/home/css/nav.css">
+	<link rel="stylesheet" type="text/css" href="/restaurant1/restaurant/Public/home/css/index.css">
+	<link rel="stylesheet" type="text/css" href="/restaurant1/restaurant/Public/home/css/about.css">
+	<link rel="stylesheet" type="text/css" href="/restaurant1/restaurant/Public/home/css/manage.css">
+	<link rel="stylesheet" type="text/css" href="/restaurant1/restaurant/Public/home/css/menu-content.css">
+	<link rel="stylesheet" type="text/css" href="/restaurant1/restaurant/Public/home/css/menu.css">
+	<link rel="stylesheet" type="text/css" href="/restaurant1/restaurant/Public/home/css/news.css">
+	<link rel="stylesheet" type="text/css" href="/restaurant1/restaurant/Public/home/css/order.css">
+	<link rel="stylesheet" type="text/css" href="/restaurant1/restaurant/Public/home/css/person.css">
+	<link rel="stylesheet" href="/restaurant1/restaurant/Public/home/css/bootstrap.css">
+	<script src="/restaurant1/restaurant/Public/home/js/jquery-1.11.3.js"></script>
+	<script src="/restaurant1/restaurant/Public/home/js/bootstrap.js"></script>
+	<script type="text/javascript" src="/restaurant1/restaurant/Public/home/js/index.js"></script>
+	<script type="text/javascript" src="/restaurant1/restaurant/Public/home/js/news.js"></script>
+</head>
+<body>
+	<div class="head">
+		<div class="head-left">
+			<ul class="xm-nav">
+				<li><img src="/restaurant1/restaurant/Public/home/images/ms03.png" width="100%" height="100%">
+				<li><a href="<?php echo U('Home/Index/index');?>">首页</a></li>
+				<li><a href="<?php echo U('Home/Order/order');?>">订单查询</a></li>
+				<li><a href="<?php echo U('Home/Person/person');?>">个人中心</a></li>
+				<li><a href="<?php echo U('Home/News/news');?>">新闻中心</a></li>
+				<li><a href="<?php echo U('Home/About/about');?>">关于我们</a></li>
+				<div class="clear"></div>
+			</ul>
+		</div>
+		<div class="head-right">
+			<div class="xm-sign">
+				<a href="<?php echo U('Home/Login/login');?>">登录</a>/<a href="<?php echo U('Home/Login/login');?>">注册</a>
+			</div>
+		</div>
+		<div class="clear"></div>
+	</div>
+	<!--主体部分 开始-->
+	<div class="page-theme">
+		<!--导航 开始-->
+		<div class="nav">
+			<ul class="nav-theme">
+				<li class="nav-theme-logo">
+					<a href="javascript: void (0);">
+						<img src="/restaurant1/restaurant/Public/home/images/ms02.png" alt="" width="100%" height="100%">
+					</a>
+				</li>
+				<li class="nav-theme-nav">
+					<a href="<?php echo U('Home/Index/index');?>">首页</a>
+					<a href="<?php echo U('Home/Order/order');?>">订单查询</a>
+					<a href="<?php echo U('Home/Person/person');?>">个人中心</a>
+					<a href="<?php echo U('Home/News/news');?>">新闻中心</a>
+					<a href="<?php echo U('Home/About/about');?>">关于我们</a>
+				</li>
+				<li class="nav-theme-search">
+					<div class="nav-theme-search-input">
+						<input type="text">
+						<a href="javascript: void (0);">搜索</a>
+					</div>
+				</li>
+				<div class="clear"></div>
+			</ul>
+		</div>
+		
+<div class="ne_content">
+	<h1>血液不流畅，8种食物活血化瘀</h1>
+	<p>人的血液畅通，身体才能健康，如果血液处于高凝状态或是血流迟缓，血管壁不光滑，就会导致瘀血阻滞，血液流通不畅、阻滞就会成为健康的隐形杀手，从而产生很多的疾病，所以及时活血化瘀十分的重要。日常生活中有很多食物活血化瘀的效果都很好，可以有效通畅体内血脉、去除淤血，瘀血体质的人群，可以常食。</p>
+</div>
+<div class="ne_foot">
+	<ul>
+		<li><a href="#">上一篇:</a></li>
+		<li><a href="#">下一篇:</a></li>
+	</ul>
+</div>
+</div>
+		<div class="footers">
+		<div class="footer">
+			<div class="footer-left">
+				<p class="feel">职工餐厅-公司给您家的感觉</p>
+				<ul>
+					<li><a href="<?php echo U('Home/Index/index');?>">首页</a></li>
+					<li><a href="<?php echo U('Home/Order/order');?>">订单查询</a></li>
+					<li><a href="<?php echo U('Home/Person/person');?>">个人中心</a></li>
+					<li><a href="<?php echo U('Home/News/news');?>">新闻中心</a></li>
+					<li><a href="<?php echo U('Home/About/about');?>">关于我们</a></li>
+					<li class="clear"></li>
+				</ul>
+				<p>联系电话：0000-000000</p>
+			</div>
+			<div class="footer-right">
+				<p class="feel">餐厅展景</p>
+				<ul>
+					<li><img src="/restaurant1/restaurant/Public/home/images/restaurant/1.jpg"></li>
+					<li><img src="/restaurant1/restaurant/Public/home/images/restaurant/1.jpg"></li>
+					<li><img src="/restaurant1/restaurant/Public/home/images/restaurant/1.jpg"></li>
+					<li><img src="/restaurant1/restaurant/Public/home/images/restaurant/1.jpg"></li>
+					<div class="clear"></div>
+				</ul>
+			</div>
+			<div class="clear"></div>
+		</div>
+	</div>
+</body>
+</html>
